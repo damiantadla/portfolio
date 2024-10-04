c55c3ddb3b53e50dbf19a5ef22f8969e4c70f8eb
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-10-05 00:02:43 +0200

    edit page.tsx

--- frontend/portfolio/src/app/page.tsx
+++ frontend/portfolio/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Damian Tadla.</li>
+          <li>Coś innego :D</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
