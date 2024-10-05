40ec05f066430d322b73d35dd10a7d5506f61a13
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-10-05 16:17:53 +0200

    edit page.tsx

--- frontend/portfolio/src/app/page.tsx
+++ frontend/portfolio/src/app/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Hejka 2:) </li>
+          <li>Hejka :) </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
