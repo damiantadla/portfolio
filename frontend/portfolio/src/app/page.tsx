5f725b6c58435cc0b9fb012e348268c6963eb255
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-10-18 01:52:35 +0200

    Edit components

--- frontend/portfolio/src/app/page.tsx
+++ frontend/portfolio/src/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from "next/image";
 import HeaderView from "@/views/header/HeaderView"
-import ServicesComponent from "@/components/servicesComponent/ServicesComponent"
 import SectionWorkExp from "@/views/sectionWorkExp/SectionWorkExp";
 import SectionMyServices from "@/views/sectionMyServices/SectionMyServices";
-import FooterView from "@/views/footer/FooterView";
 import SectionContact from "@/views/sectionContact/SectionContact";
-import SectionSendEmail from "@/views/sectionSendEmail/SectionSendEmail";
 import SectionResults from "@/views/sectionResults/SectionResults";
 import SectionPortfolio from "@/views/sectionPortfolio/SectionPortfolio";
 
