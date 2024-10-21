00b5a60d9554061dbc27b9351bee320d25ba44c4
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-10-22 00:39:01 +0200

    Edit components

--- frontend/portfolio/src/app/resume/page.tsx
+++ frontend/portfolio/src/app/resume/page.tsx
@@ -1,16 +1,4 @@
-import HeaderView from "@/views/header/HeaderView";
-import SectionMyServices from "@/views/sectionMyServices/SectionMyServices";
-import SectionWorkExp from "@/views/sectionWorkExp/SectionWorkExp";
-import SectionContact from "@/views/sectionContact/SectionContact";
-import SectionResults from "@/views/sectionResults/SectionResults";
-import { Viewer } from '@react-pdf-viewer/core';
-import ArticleIcon from '@mui/icons-material/Article';
-// Import styles
-import '@react-pdf-viewer/core/lib/styles/index.css';
-import '@react-pdf-viewer/default-layout/lib/styles/index.css';
-
 export default function Home() {
-    // Use the correct path for the PDF
     const pdfUrl = "/WZÓR-UCHWAŁY-monitoring-garaż-WM-CT-2024 (1).pdf";
 
     return (
