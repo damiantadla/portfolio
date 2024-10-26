97869be38b90838b365e292bc91b61d844f1f06c
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-10-26 22:38:36 +0200

    Edit components

--- frontend/portfolio/src/app/layout.tsx
+++ frontend/portfolio/src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import NavComponent from "@/components/nav/NavComponent";
 import SectionSendEmail from "@/views/sectionSendEmail/SectionSendEmail";
 import FooterView from "@/views/footer/FooterView";
-import ChatBot from "@/components/chatBot/chatBot";
+
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -38,7 +38,6 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
       <NavComponent />
-      <ChatBot />
         {children}
       <SectionSendEmail/>
       <FooterView/>
