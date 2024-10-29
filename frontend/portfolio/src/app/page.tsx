c7ca8c4b5ff1332d28fe24642062ee17f4d6630f
damiantadla <157707788+damiantadla@users.noreply.github.com>
2024-10-29 10:15:35 +0100

    Edit components

--- frontend/portfolio/src/app/page.tsx
+++ frontend/portfolio/src/app/page.tsx
@@ -5,10 +5,6 @@ import SectionMyPortfolio from "@/views/sectionMyServices/SectionMyPortfolio";
 import SectionContact from "@/views/sectionContact/SectionContact";
 import SectionResults from "@/views/sectionResults/SectionResults";
 import SectionPortfolio from "@/views/sectionPortfolio/SectionPortfolio";
-import dynamic from 'next/dynamic';
-
-// Dynamiczny import ReactPlayer z wyłączonym SSR
-const ReactPlayer = dynamic(() => import("react-player/youtube"), { ssr: false });
 
 export default function Home() {
     return (
